06719ec0473872cf41c9c880638e56b2814f8637
Will Hodge <will.hodge18@gmail.com>
2017-07-25 16:43:58 -0400

    Modularity

---
+++ script.js
@@ -0,0 +1,15 @@
+/* JavaScript for hide function on site */
+
+function hiding(x, y) {
+    var hide = document.getElementById(y)
+    hide.style.display = 'none';
+    var show = document.getElementById(x);
+    if (show.style.display != 'block'){
+        show.style.display = 'none';
+    }
+    if (show.style.display == 'none') {
+        show.style.display = 'block';
+    } else {
+        show.style.display = 'none';
+    }
+}
