8b2f318ca0235111a17699fc149e40440c0e75b0
Will Hodge <will.hodge18@gmail.com>
2017-10-14 17:13:27 -0400

    integrating Node + React

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import config from './config';
+
+const server = express();
+
+server.use(express.static(__dirname + '/public'))
+  .use(cookieParser())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+
+server.get('/', (req, res) => {
+  res.render('index', {
+  });
+});
+
+
+
+server.listen(config.port, () => {
+  console.info('Express listening on port ', config.port);
+});
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /(node_modules)/,
+      }
+    ]
+  },
+};
