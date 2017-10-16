f553ff8302cc97704306e51576a0b08eed0c3521
Will Hodge <will.hodge18@gmail.com>
2017-10-15 20:01:13 -0400

    new component
    WIP

---
+++ src/components/MobileHeaderCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, Grid, Icon, Image } from 'semantic-ui-react';
+import will from '../images/will.jpg';
+
+const MobileHeaderCard = () => (
+
+);
+
+export default MobileHeaderCard;
